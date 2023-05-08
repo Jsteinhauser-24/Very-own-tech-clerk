e116b48f3453a29f97ffada2b6fec3ac0c4887f5
Jms147 <steinhauserjames3@gmail.com>
2023-05-08 15:51:02 -0700

    got all the buttons to work

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const dbData = require('./db/db.json');
 const PORT = 3001;
 const fs = require('fs');
-const uuid = require('uuid')
+const { v4: uuidv4 } = require('uuid')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,33 +16,41 @@ app.get('/notes', (req, res) =>
 );
 
 //get /api/notes should read db.json
-app.get('/api/notes', (req, res) => res.json(dbData));
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if(err) {
+            console.log(err);
+            return res.status(500).json('error');
+        }
+        res.json(JSON.parse(data));
+    });
+});
 //post /api/notes should receive new note to save on body, add to db.json then return new note
 app.post('/api/notes', (req, res) => {
     //res.json(`${req.method} request received`);
     //console.info(req.rawHeaders);
     console.info(`${req.method} request received to add note`);
-    const { noteTitle, noteText } = req.body;
-    if (noteTitle && noteText) {
+    const { title, text } = req.body;
+    if (title && text) {
         const newNote = {
-            noteTitle,
-            noteText,
-            note_id: uuid(),
+            title,
+            text,
+            id: uuidv4(),
         };
 
-        fs.readFile('./db/dbData.json', 'utf8', (err, data) => {
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
-                console.error(err);
+                console.log(err);
             } else {
                 const parsedNotes = JSON.parse(data);
 
                 parsedNotes.push(newNote)
 
-                const noteString = JSON.stringify(newNote)
-                fs.writeFile(`./db/${newNote.noteTitle}.json`, noteString, (err) =>
+                const noteString = JSON.stringify(parsedNotes)
+                fs.writeFile(`./db/db.json`, noteString, (err) =>
                     err
-                        ? console.error(err)
-                        : console.log(`${newNote.noteTitle} has been written to JSON file`));
+                        ? console.log(err)
+                        : console.log(`${newNote.title} note`));
             }
         })
         const result = {
@@ -57,6 +65,30 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+        return res.status(500).json('Error');
+    }    
+    const parsedNotes = JSON.parse(data);
+    const updateNotes = parsedNotes.filter((note) => note.id !== noteId);
+
+    fs.writeFile('./db/db.json', JSON.stringify(updateNotes), (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json
+        }
+        console.log(`${noteId} has been deleted`);
+        res.status(200).json({
+            status: 'Success',
+            note_id: noteId
+        });
+    });
+});
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
